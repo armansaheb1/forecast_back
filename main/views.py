dd14aadcdbe174fe9597553d5aec07978bfe392c
armansaheb1 <armansaheb@gmail.com>
2025-02-09 22:27:06 +0100

    Update

--- main/views.py
+++ main/views.py
@@ -1,42 +1,66 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from . import models
 import openai
 import os
+import json
+
+ROOT = "http://10.45.190.255:8000/media/"
+
 # Create your views here.
-# os.environ['OPENAI_API_KEY'] = 
+os.environ["OPENAI_API_KEY"] = (
+    "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"
+)
+
+
 class GBuilderFile(APIView):
 
     def post(self, request):
-        completion = openai.chat.completions.create( model="gpt-4o",
-        messages=[ {"role": "system", "content": "You are a helpful assistant capable of reading coffee cups and fortune telling with it as well"}, 
-                    { "role": "user", "content": [
+        data = request.data.get("images")
+        file = models.File.objects.create(image=data)
+        data = []
+        data.append(ROOT + file.image.name)
+        print(data)
+        completion = openai.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant capable of reading coffee cups and fortune telling with it as well",
+                },
+                {
+                    "role": "user",
+                    "content": [
                         {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": request.data['file'][0]
+                            "type": "image_url",
+                            "image_url": {"url": data[0]},
                         }
-                        }
-                    ] 
-                    },
-                    { "role": "user", "content": [
+                    ],
+                },
+                {
+                    "role": "user",
+                    "content": [
                         {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": request.data['file'][1]
+                            "type": "image_url",
+                            "image_url": {"url": data[0]},
                         }
-                        }
-                    ] 
-                    },
-                    { "role": "user", "content": [
+                    ],
+                },
+                {
+                    "role": "user",
+                    "content": [
                         {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": request.data['file'][2]
+                            "type": "image_url",
+                            "image_url": {"url": data[0]},
                         }
-                        }
-                    ] 
-                    },
-                   {"role": "user", "content": "بر اساس عکس های داده شده فال قهوه بسیار کامل و طولانی را ایجاد کن و بدون هیچ توضیح و اضافاتی بده"} ] ) 
-        print(completion.choices[0].message) 
+                    ],
+                },
+                {
+                    "role": "user",
+                    "content": "بر اساس عکس های داده شده فال قهوه بسیار کامل و طولانی را ایجاد کن و بدون هیچ توضیح و اضافاتی بده",
+                },
+            ],
+        )
+        print(completion.choices[0].message)
         return Response(completion.choices[0].message)
